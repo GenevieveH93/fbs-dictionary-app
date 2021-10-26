81306d5e3e8af0c256513a852533f95c17892cb7
Genevieve <genevievehines@gmail.com>
2021-10-26 18:53:09 +1100

    Created the app structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <p>Dictionary</p>
+    </div>
+  );
+}
+
+export default App;
